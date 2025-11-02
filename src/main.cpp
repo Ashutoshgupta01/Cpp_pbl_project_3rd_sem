37756fd112cc73c50f558a25debb1e97ab79c050
Ashutosh Gupta <109278813+Ashutoshgupta01@users.noreply.github.com>
2025-11-02 15:35:10 +0530

    update the project
    Move the files to the SRC folder and format and improve the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
-#include "structures.cpp"   
+#include "structures.cpp"
 
 using namespace std;
 
@@ -17,26 +17,37 @@ private:
 
 public:
     Theatre() {
+        // Initialize movie list
         movies.push_back({1, "Avengers: Endgame", "Action/Sci-Fi", "10:00 AM"});
         movies.push_back({2, "Inception", "Sci-Fi/Thriller", "1:30 PM"});
         movies.push_back({3, "The Lion King", "Animation/Drama", "5:00 PM"});
+        movies.push_back({4, "Interstellar", "Adventure/Sci-Fi", "8:00 PM"});
+        movies.push_back({5, "Joker", "Psychological/Drama", "10:30 PM"});
+        movies.push_back({6, "Avatar: The Way of Water", "Adventure/Fantasy", "12:00 PM"});
 
+        // Initialize food menu
         menu.push_back({1, "Popcorn", 100});
         menu.push_back({2, "Cold Drink", 80});
         menu.push_back({3, "Nachos", 120});
         menu.push_back({4, "Burger", 150});
-        menu.push_back({5, "Pizza Slice", 00});
+        menu.push_back({5, "Pizza Slice", 200});
+        menu.push_back({6, "French Fries", 90});
+        menu.push_back({7, "Ice Cream", 60});
+        menu.push_back({8, "Coffee", 70});
+        menu.push_back({9, "Sandwich", 110});
+        menu.push_back({10, "Chocolate Bar", 50});
 
+        // Initialize all seats as available
         for (int i = 0; i < ROWS; i++)
             for (int j = 0; j < COLS; j++)
                 seats[i][j] = 'O';
     }
 
     void showMovies() {
-        cout << "\n===== Available Movies =====\n";
+        cout << "\n========== AVAILABLE MOVIES ==========\n";
         for (auto &m : movies) {
-            cout << m.id << ". " << m.name << " (" << m.genre 
-                 << ") - " << m.time << endl;
+            cout << setw(2) << m.id << ". " << m.name 
+                 << " (" << m.genre << ") - " << m.time << endl;
         }
     }
 
@@ -48,7 +59,8 @@ public:
         for (auto &m : movies) {
             if (m.id == choice) {
                 selectedMovie = m;
-                cout << "\nYou selected: " << m.name << " (" << m.genre << ") at " << m.time << endl;
+                cout << "\nYou selected: " << m.name 
+                     << " (" << m.genre << ") at " << m.time << endl;
                 return;
             }
         }
@@ -57,13 +69,13 @@ public:
     }
 
     void displaySeats() {
-        cout << "\n===== Seat Layout (O = Available, X = Booked) =====\n\n   ";
+        cout << "\n========== SEAT LAYOUT (O = Available, X = Booked) ==========\n\n   ";
         for (int j = 0; j < COLS; j++)
-            cout << setw(2) << j+1 << " ";
+            cout << setw(2) << j + 1 << " ";
         cout << endl;
 
         for (int i = 0; i < ROWS; i++) {
-            cout << setw(2) << i+1 << " ";
+            cout << setw(2) << i + 1 << " ";
             for (int j = 0; j < COLS; j++) {
                 cout << " " << seats[i][j] << " ";
             }
@@ -143,11 +155,12 @@ public:
     }
 
     void generateBill() {
-        cout << "\n===== TICKET =====\n";
-        cout << "Movie: " << selectedMovie.name << " (" << selectedMovie.genre << ")\n";
-        cout << "Time: " << selectedMovie.time << "\n";
+        cout << "\n=================== TICKET ===================\n";
+        cout << "Movie: " << selectedMovie.name 
+             << " (" << selectedMovie.genre << ")\n";
+        cout << "Show Time: " << selectedMovie.time << "\n";
         cout << "Seats: ";
-        for (auto &s : bookedSeats) cout << s << ", ";
+        for (auto &s : bookedSeats) cout << s << "  ";
         cout << "\nTicket Price: Rs. " << bookedSeats.size() * TICKET_PRICE << endl;
 
         if (!orderedFood.empty()) {
@@ -157,8 +170,9 @@ public:
             }
         }
 
-        cout << "\nTOTAL: Rs. " << totalAmount << endl;
-        cout << "===================\n";
+        cout << "\n----------------------------------------------\n";
+        cout << "TOTAL AMOUNT: Rs. " << totalAmount << endl;
+        cout << "==============================================\n";
     }
 };
 
